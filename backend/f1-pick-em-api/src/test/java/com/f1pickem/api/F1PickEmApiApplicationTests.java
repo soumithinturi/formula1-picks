298d19b1006d4a9f0019d78ce42c6beeaa4b367e
soumithinturi <sintur2527@gmail.com>
2025-08-24 22:47:52 -0500

    Initialize backend

---
+++ backend/f1-pick-em-api/src/test/java/com/f1pickem/api/F1PickEmApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.f1pickem.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class F1PickEmApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
